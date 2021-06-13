1cfc7f1e0ac30957f5bb25b92b44128f1bde19e5
Banauag <hera.n.m.banauag@accenture.com>
2021-06-13 16:11:17 +0800

    test case #1 is done

--- CypressTest/assessment/test.case.one.spec.js
+++ CypressTest/assessment/test.case.one.spec.js
@@ -1,11 +1,30 @@
 describe('Test Case 1', function() {
 
+    // From the home page go to contact page
     before(() => {
-        cy.visit('https://www.google.com');
-        cy.contains('Google offered in:').should('exist');
+        cy.contact_page()
+        cy.contains('Forename')
     })
     
-    it('Search field', () => {
-       cy.get('input[title=Search]').click().type('hera');
+    it('Click submit button', () => {
+       cy.contains('Submit').should('exist').click()
+    })
+
+    it('Validate errors', () => {
+        cy.contains('Forename is required').should('exist')
+        cy.contains('Email is required').should('exist')
+        cy.contains('Message is required').should('exist')
+    })
+
+    it('Populate mandatory fields', () => {
+        cy.get('input[id=forename]').type('hera')
+        cy.get('input[id=email]').type('hera.nielsen@example.com')
+        cy.get('#message').type('message')
+    })
+
+    it('Validate errors are gone', () => {
+        cy.contains('Forename is required').should('not.exist')
+        cy.contains('Email is required').should('not.exist')
+        cy.contains('Message is required').should('not.exist')
     })
 })
--- CypressTest/cypress/support/commands.js
+++ CypressTest/cypress/support/commands.js
@@ -23,3 +23,6 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add("contact_page", () => {
+    cy.visit('https://jupiter.cloud.planittesting.com/#/contact')
+})
